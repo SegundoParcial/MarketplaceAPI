b62ede0a02767707da63f8d1812fe89349018c49
Nicolas Tineo <a2024116302@estudiantes.upsa.edu.bo~>
2025-10-27 21:11:18 -0400

    nuevos cambios 2

---
+++ Migrations/20251028004040_AddMigrationsFinal.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarketplaceAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMigrationsFinal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
